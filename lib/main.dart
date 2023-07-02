e3c69f91672db1374542f3ceb0c545a0d386266e
Anmol Anand <76034132+anmol420@users.noreply.github.com>
2023-07-02 06:09:48 +0530

    Added Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:gk_app/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Text(
-          "Hello !",
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(222, 203, 164,0.9),
+                Color.fromRGBO(62, 81, 81,0.7),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
         ),
       ),
     ),
